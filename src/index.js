b0204d9ecaedd2d1beda636c48334bc34dc923c5
asseternity <nakupovasset@gmail.com>
2024-05-27 17:41:35 +0500

    feat: html elements init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import "./styles.css";
+import portrait1_img from "./images/cottonbro.jpg";
+import portrait2_img from "./images/cottonbro2.jpg";
 
-const sum = (num1, num2) => {
-  return num1 + num2;
-};
+let portrait1_area = document.querySelector(".portrait");
+let portrait1_element = document.createElement("img");
+portrait1_element.src = portrait1_img;
+portrait1_area.appendChild(portrait1_element);
 
-export default sum;
+let portrait2_area = document.querySelector(".bottom_portrait");
+let portrait2_element = document.createElement("img");
+portrait2_element.src = portrait2_img;
+portrait2_area.appendChild(portrait2_element);
--- src/index.test.js
+++
@@ -1,5 +0,0 @@
-import sum from "./index";
-
-test("test sum", () => {
-  expect(sum(1, 5)).toBe(6);
-});
